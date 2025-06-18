2111334f067f4ee568ca71fa575479d0560eb74b
rishikpulhani <rishikpulhani@gmail.com>
2025-06-19 02:25:01 +0530

    made a bare metal binary

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(_info : &PanicInfo) -> !{
+    loop {}
+}
+// no need to mark the panic function with no_mangle as it is not refered by its name while linking instead it is marked as the panic handler to identify it unique;y
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> !{
+    // here this is a rust function which is accessed from outside but this happens at the c abi 
+    loop {}
+}
