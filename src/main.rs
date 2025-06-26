f2ec953fd8b620f58923ca594a828dd8df34da73
Rishikpulhani <rishik_rkp@ma.iitr.ac.in>
2025-06-27 01:53:44 +0530

    finished blog 2

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,17 @@ fn panic(_info : &PanicInfo) -> !{
     loop {}
 }
 // no need to mark the panic function with no_mangle as it is not refered by its name while linking instead it is marked as the panic handler to identify it unique;y
+static HELLO: &[u8] = b"Hello World!";
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> !{
     // here this is a rust function which is accessed from outside but this happens at the c abi 
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
